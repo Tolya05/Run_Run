fe28b28f9e1941f34f3aa21700acbe4f3fba16fe
Tolya05 <72998652+Tolya05@users.noreply.github.com>
2023-01-28 05:46:49 -0500

    Basic fils

---
+++ Run_Run.nimble
@@ -0,0 +1,14 @@
+# Package
+
+version       = "0.1.0"
+author        = "Tolya05"
+description   = "A Great Endless Runner Game with Raylib in Nim"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["Run_Run"]
+
+
+# Dependencies
+
+requires "nim >= 1.6.10"
+requires "nimraylib_now"
---
+++ src/Run_Run.nim
@@ -0,0 +1,32 @@
+import nimraylib_now
+
+func DrawGame(PlayerPos: Vector2, PlayerSize: Vector2) =
+    beginDrawing()
+    clearBackground(White)
+
+    drawRectangleV(PlayerPos, PlayerSize, Black)
+
+    endDrawing()
+
+proc GameLogic(Player: var Vector2, PlayerSize: Vector2) =
+  let SPEED: float32 = 250
+  let delta = getFrameTime()
+
+  if isKeyDown(D):
+    Player.x += SPEED * delta
+  if isKeyDown(A):
+    Player.x -= SPEED * delta
+  DrawGame(Player, PlayerSize)
+    
+
+func main() =
+  initWindow(800, 400, "Run! Run!")
+
+  var Player: Vector2 = (400.0, 200.0)
+  var PlayerSize: Vector2 = (50.0, 80.0)
+
+  while not windowShouldClose():
+    GameLogic(Player, PlayerSize)
+
+when isMainModule:
+  main()
